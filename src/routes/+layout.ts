99ce0f4620ebdc04602597eafd7b794517357a6c
Azmal Akhtar <azmalakhtar04@gmail.com>
2025-07-12 16:55:10 +0530

    try-1

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -13,4 +13,4 @@ export const load: LayoutLoad = ({ url }) => {
 }
 
 // This can be false if you're using a fallback (i.e. SPA mode)
-export const prerender = true;
+export const prerender = false;
